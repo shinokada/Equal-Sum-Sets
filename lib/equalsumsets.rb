157d0e35b783a9605472a007e46a849dd813dfe7
shinokada <okada.shin@gmail.com>
2014-04-18 22:44:47 +0900

    Added class and method

--- lib/equalsumsets.rb
+++ lib/equalsumsets.rb
@@ -1,6 +1,12 @@
 class Equalsumsets
 
-
+  # Find number of set which equals to s
+  # @param n [Fixnum] Maximum number
+  # @param k [Fixnum] Number of set elements
+  # @param s [Fixnum] Sum of elements
+  def self.find_set(n, k, s)
+    (1..n).to_a.combination(k).to_a.map{ |item| item.inject(:+) }.find_all{ |i| i==s }.count
+  end
 
 
 end
--- spec/lib/equalsumsets_spec.rb
+++ spec/lib/equalsumsets_spec.rb
@@ -1,7 +1,15 @@
-require './spec_helper'
-require './equalsumsets.rb'
+require 'spec_helper'
 
 describe Equalsumsets do
 
+  specify { expect(Equalsumsets.find_set(9, 3, 23)). to eq 1 }
+  specify { expect(Equalsumsets.find_set(9, 3, 22)). to eq 2 }
+  specify { expect(Equalsumsets.find_set(10, 3, 28)). to eq 0 }
+  specify { expect(Equalsumsets.find_set(16, 10, 107)). to eq 20 }
+  specify { expect(Equalsumsets.find_set(20, 8, 102)). to eq 1542 }
+  specify { expect(Equalsumsets.find_set(20, 10, 105)). to eq 5448 }
+  specify { expect(Equalsumsets.find_set(20, 10, 155)). to eq 1 }
+  specify { expect(Equalsumsets.find_set(3, 4, 3)). to eq 0 }
+  specify { expect(Equalsumsets.find_set(4, 2, 11)). to eq 0 }
 
 end
