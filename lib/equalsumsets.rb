9e7f8a59baa8c0add164ad8cc710b0aba5423dce
shinokada <okada.shin@gmail.com>
2014-04-18 22:47:56 +0900

    Added doc

--- lib/equalsumsets.rb
+++ lib/equalsumsets.rb
@@ -1,12 +1,11 @@
+# The output for each dataset should be a line containing a single integer that gives the number of thesets that satisfy the conditions.
 class Equalsumsets
-
   # Find number of set which equals to s
   # @param n [Fixnum] Maximum number
   # @param k [Fixnum] Number of set elements
   # @param s [Fixnum] Sum of elements
+  # @return [Fixnum] number of solution
   def self.find_set(n, k, s)
     (1..n).to_a.combination(k).to_a.map{ |item| item.inject(:+) }.find_all{ |i| i==s }.count
   end
-
-
 end
--- spec/lib/equalsumsets_spec.rb
+++ spec/lib/equalsumsets_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Equalsumsets do
-
   specify { expect(Equalsumsets.find_set(9, 3, 23)). to eq 1 }
   specify { expect(Equalsumsets.find_set(9, 3, 22)). to eq 2 }
   specify { expect(Equalsumsets.find_set(10, 3, 28)). to eq 0 }
@@ -11,5 +10,4 @@ describe Equalsumsets do
   specify { expect(Equalsumsets.find_set(20, 10, 155)). to eq 1 }
   specify { expect(Equalsumsets.find_set(3, 4, 3)). to eq 0 }
   specify { expect(Equalsumsets.find_set(4, 2, 11)). to eq 0 }
-
 end
